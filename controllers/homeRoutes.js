6253f426a07491c7e8c172b692e8f2ed74af7053
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-26 21:14:27 -0700

    started routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,5 +2,13 @@ const router = router('express').Router();
 const { Post, User } = require('../models');
 
 // GET all post for homepage
-router.get
+router.get('/', async (req, res) => {
 
+});
+
+// GET one post 
+router.get('/post/:id', async (req, res) => {
+
+});
+
+module.exports = router;
