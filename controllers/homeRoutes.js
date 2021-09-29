adcd84cdeadd740fc65b67cd3d93a6dea66f19ca
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-28 21:04:07 -0700

    added homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,42 @@
 const router = router('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 // const withAuth = require('')
 
-// GET all post for homepage
+// GET route for homepage
 router.get('/', async (req, res) => {
-
+  try {
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn
+    });
+    res.status(200);
+  } catch(err) {
+    res.status(500).json(err)
+  }
 });
 
-// GET one post 
-router.get('/post/:id', async (req, res) => {
-
+// GET route for login
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
+  } catch {
+    res.status(500).json(err)
+  }
 });
+
+// GET route to access dashboard
+router.get('/dashboard', async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+    const post = postData.map((post) => post.get({ plain: true}));
+
+    res.render('dashboard', {
+      post, 
+      loggedIn: req.session.loggedIn
+    });
+  } catch {
+    res.status(500).json(err);
+  }
+})
+
 
 module.exports = router;
